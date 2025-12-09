b184cd5c4a28a99c650214ee3c39b260a7c45249
Bogyeong Kim <bgk614@gmail.com>
2025-12-09 18:10:49 +0900

    feat: 유저 라우터 수정

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -2,7 +2,16 @@ import express from 'express';
 
 import { userController } from '../controllers/user.controller.js';
 import { authMiddleware } from '../middlewares/auth.middleware.js';
+import { validateMiddleware } from '../middlewares/validate.middleware.js';
 
 export const userRouter = express.Router();
 
-userRouter.get('/me', authMiddleware, userController.getById);
+userRouter.route('/').post(validateMiddleware); // 회원가입
+
+userRouter
+  .route('/me')
+  .get(authMiddleware, userController.getById) // 내 정보 조회
+  .patch(authMiddleware, validateMiddleware); // 내 정보 수정
+
+userRouter.get('/me/likes', authMiddleware); // 관심 스토어 조회
+userRouter.delete('/delete', authMiddleware); // 회원 탈퇴
