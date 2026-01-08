f16cae33678a234c6fdcb7f5baf23741eec4d041
이서진 <qps0211@naver.com>
2026-01-08 18:09:14 +0900

    [Refactor] : Dashboard API 아키텍처 리팩토링 (Repository 분리, return 제거)

--- src/controllers/dashboard.controller.ts
+++ src/controllers/dashboard.controller.ts
@@ -1,25 +1,16 @@
 import type { NextFunction, Request, Response } from 'express';
 
-import { MESSAGE, STATUS_CODE } from '../constants/constant.js';
+import { STATUS_CODE } from '../constants/constant.js';
 import { dashboardService } from '../services/dashboard.service.js';
-import { HttpException } from '../utils/http-exception.js';
 
 class DashboardController {
-  private getUserIdOr401(req: Request): string {
-    const userId = req.user?.id;
-    if (!userId) {
-      throw HttpException.unauthorized(MESSAGE.unauthorized);
-    }
-    return userId;
-  }
-
   getDashboard = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const userId = this.getUserIdOr401(req);
+      const userId = req.user!.id;
       const dashboard = await dashboardService.getDashboard(userId);
-      return res.status(STATUS_CODE.OK).json(dashboard);
+      res.status(STATUS_CODE.OK).json(dashboard);
     } catch (err) {
-      return next(err);
+      next(err);
     }
   };
 }
